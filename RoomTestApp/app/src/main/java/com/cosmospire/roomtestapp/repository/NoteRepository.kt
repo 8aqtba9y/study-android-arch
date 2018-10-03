8f3d574e68a3e60f9d0526b165ae0fc6f47384ff
8aqtba9y <8aqtba9y@gmail.com>
2018-10-04 02:20:18 +0900

    [add] repository with async gradle

---
+++ RoomTestApp/app/src/main/java/com/cosmospire/roomtestapp/repository/NoteRepository.kt
@@ -0,0 +1,45 @@
+package com.cosmospire.roomtestapp.repository
+
+import android.app.Application
+import android.arch.lifecycle.LiveData
+import com.cosmospire.roomtestapp.room.Note
+import com.cosmospire.roomtestapp.room.NoteDao
+import com.cosmospire.roomtestapp.room.NoteDatabase
+import org.jetbrains.anko.doAsync
+
+
+class NoteRepository(application: Application) {
+    lateinit var noteDao: NoteDao
+    lateinit var allNotes: LiveData< List<Note> >
+
+    init {
+        val database: NoteDatabase = NoteDatabase.getInstance(application)
+        noteDao = database.noteDao()
+        allNotes = noteDao.getAllNotes()
+    }
+
+    fun insert(note: Note) {
+        doAsync {
+            noteDao.insert(note)
+        }
+    }
+
+    fun update(note: Note) {
+        doAsync {
+            noteDao.update(note)
+        }
+    }
+
+    fun delete(note: Note) {
+        doAsync {
+            noteDao.delete(note)
+        }
+    }
+
+    fun deleteAllNotes() {
+        doAsync {
+            noteDao.deleteAllNotes()
+        }
+    }
+
+}
