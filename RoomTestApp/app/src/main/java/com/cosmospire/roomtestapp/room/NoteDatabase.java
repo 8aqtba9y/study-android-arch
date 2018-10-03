853281f8712ab1984573c7f6503ba25a71c6250d
8aqtba9y <8aqtba9y@gmail.com>
2018-10-04 02:36:45 +0900

    [add] callback onCreate

--- RoomTestApp/app/src/main/java/com/cosmospire/roomtestapp/room/NoteDatabase.java
+++ RoomTestApp/app/src/main/java/com/cosmospire/roomtestapp/room/NoteDatabase.java
@@ -1,10 +1,14 @@
 package com.cosmospire.roomtestapp.room;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
+// TODO: convert in kotlin
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
 
@@ -18,8 +22,38 @@ public abstract class NoteDatabase extends RoomDatabase {
                     NoteDatabase.class,
                     "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private  NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db) {
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title1", "Description1", 1));
+            noteDao.insert(new Note("Title2", "Description2", 2));
+            noteDao.insert(new Note("Title3", "Description3", 3));
+            return null;
+        }
     }
 }
